7371d47494829ea0d8078fbc0732e814b5cabed6
Dustin Brown <me@dubrowgn.com>
2022-03-06 10:15:57 -0700

    Build non-rc 3.10 package

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
-for version in "3.6" "3.7" "3.8" "3.9" "3.10-rc"; do
-	sudo docker run \
-		-it \
-		--rm \
-		-v "$(pwd):/mnt" \
-		-w /mnt \
-		"python:$version-alpine" \
-		python3 ./setup.py bdist_egg
+for version in "3.6" "3.7" "3.8" "3.9" "3.10"; do
+	img="python:$version-alpine"
+
+	sudo docker pull "$img" \
+		&& sudo docker run \
+			-it \
+			--rm \
+			-v "$(pwd):/mnt" \
+			-w /mnt \
+			"$img" \
+			python3 ./setup.py bdist_egg \
+		|| exit
 done
 
 sudo chown -R "$(id -u):$(id -g)" build dist RcData.egg-info
